e298b68ef70f9dcb69205646c5c5c023282d1a6e
Anselmo Barbosa <anselmojacyntho@gmail.com>
2022-04-13 17:24:00 -0400

    feat(): Presence command implematation

--- app/Commands/PresenceManager.php
+++ app/Commands/PresenceManager.php
@@ -3,11 +3,45 @@
 namespace App\Commands;
 
 use App\Contracts\CommandContract;
+use App\Traits\FileManager;
+use App\Traits\DataManipulator;
+use PhpParser\Node\Stmt\Foreach_;
 
 class PresenceManager implements CommandContract {
+    
+    use FileManager, DataManipulator;
 
-    public function run($args)
+    protected $command = 'Presence';
+
+    public $storageFile;
+    public $content;
+
+    public function __construct()
     {
-        echo "Ola";
+        $this->storageFile = default_storage_file();
+        $this->file = $this->getContent($this->storageFile);
+    }
+
+    public function run($params)
+    {
+        $args = $params['args'];        
+        
+        if ($this->studentExists($this->file, $args[0])) {            
+            $this->insertRow($this->storageFile, $this->createRow($args)); 
+        }      
+        
+        $list = $this->classAttendanceTimeList($this->getContent($this->storageFile));
+
+        foreach($list as $line) {
+            echo "{$line['name']}: {$line['time']} \n";
+        }
+    }
+
+    public function createRow($data)
+    {        
+        return [
+            $this->command,
+            ...$data
+        ];
     }
 }
